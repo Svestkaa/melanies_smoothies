8a1d8a53c113ab8d1b2437fcdb666e3b35f2c736
Svestkaa <aiagents@centrum.cz>
2025-12-30 14:06:34 +0100

    Remove redundant API call for watermelon data

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,13 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-
 if ingredients_list:
-
     ingredients_string=''
+  
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write (ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -48,6 +48,4 @@ values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
       
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
